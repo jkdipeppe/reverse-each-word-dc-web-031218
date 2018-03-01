178d91988a23fb88c52e695c449efa0f32c61e54
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-03-01 20:49:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,14 @@
 def reverse_each_word(string)
   array = Array.new
+  array_final = Array.new
   array = string.split
+
+  array.each |word|
+    temp_word = String.new
+    word.each |letter|
+      temp_word.insert(0, letter)
+    end
+    array_final << temp_word
+  end
+  array_final
 end
